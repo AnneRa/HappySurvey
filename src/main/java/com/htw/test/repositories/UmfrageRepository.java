786654be34345788192a7b0c0d7d1ef342a75629
lawuc <lawuc@DESKTOP-SJ8V323>
2016-12-07 17:29:53 +0100

    * Fixed Swagger * implement GET, DELETE and PUSH massage * implement Model Umfrage * new structure

---
+++ src/main/java/com/htw/test/repositories/UmfrageRepository.java
@@ -0,0 +1,7 @@
+package com.htw.test.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.htw.test.model.Umfrage;
+
+public interface UmfrageRepository extends CrudRepository<Umfrage, Integer>{}
