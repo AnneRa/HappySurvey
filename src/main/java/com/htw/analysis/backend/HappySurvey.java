693a25c4331c72629fc2e643398a75342b582657
lawuc <lawuc@DESKTOP-SJ8V323>
2016-12-07 17:23:18 +0100

    Revert "* new project structure * swagger done * API for "umfrage" (GET,POST,DELETE) Done * model for "umfrage" DONE"
    This reverts commit 0ea2aae692b6c4fcb2b19adfe7d5293b6832d44b.

--- src/main/java/com/htw/analysis/backend/HappySurvey.java
+++ src/main/java/com/htw/analysis/backend/HappySurvey.java
@@ -1,13 +1,15 @@
-package com.htw.test;
+package com.htw.analysis.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HappySurvey {
-
+	
 	public static void main(String[] args) throws Throwable {
-		SpringApplication.run(HappySurvey.class, args);
+        SpringApplication.run(HappySurvey.class, args);
+    }
+
 	}
 
-}
--- src/main/java/com/htw/test/repositories/UmfrageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.htw.test.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.htw.test.model.Umfrage;
-
-public interface UmfrageRepository extends CrudRepository<Umfrage, Integer>{}
