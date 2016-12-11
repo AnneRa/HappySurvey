17cf0873a3d8566dae8e87b96a85d5f2fbe22bdc
Gesa Steensen <s0551690@htw-berlin.de>
2016-12-11 12:55:38 +0100

    Revert "Reconfiguration der Angular-Routen und index.html in resources/static"
    This reverts commit add53978b9c60725f0da5a04e195be7f77e1ce54.

--- src/main/app/app.js
+++ src/main/app/app.js
@@ -1,50 +1,24 @@
 var happySurvey = angular.module('happySurvey',['ui.router', 'ngAnimate', 'ngSanitize', 'ngMaterial']);
 
-happySurvey.config(RoutesManager);
-
-RoutesManager.$inject = ['$stateProvider', '$urlRouterProvider'];
-function RoutesManager($stateProvider, $urlRouterProvider) {
-	
-	var signinState = {
-			name: 'signin',
-			url: '/signin',
-			templateUrl: '../views/signin.template.html'
-	}
-	
-	var homeState = {
-			name: 'home',
-			url: '/',
-			templateUrl: '../index.html'
-	}
-	
-	$stateProvider.state(signinState);
-	$stateProvider.state(homeState);
-	
-	$urlRouterProvider.otherwise('/');
-	
-}
-
-
 /*funktioniert noch nicht*/
-//happySurvey.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-//	
+happySurvey.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
 	
-//	
-//	$stateProvider
-//	.state('public', {
-//		abstract:true,
-//		templateUrl:'../src/main/index.html'
-//		//template:"<ui-view/>"
-//	})
-//	.state('public.site', {
-//		url:'/start',
-//		templateUrl:'../src/main/views/hellopage.html'
-//	});
-//	
-//	$stateProvider
-//	.state('private', {
-//		abstract:true,
-//		template:"<ui-view/>"
-//	});
+	$stateProvider
+	.state('public', {
+		abstract:true,
+		templateUrl:'../src/main/index.html'
+		//template:"<ui-view/>"
+	})
+	.state('public.site', {
+		url:'/start',
+		templateUrl:'../src/main/views/hellopage.html'
+	});
 	
-//}]);
+	$stateProvider
+	.state('private', {
+		abstract:true,
+		template:"<ui-view/>"
+	});
+	
+}]);
