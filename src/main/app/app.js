110abdf0354178cfed60cdd2e48aa7fd82df37d2
Gesa Steensen <s0551690@htw-berlin.de>
2016-12-11 12:56:55 +0100

    keeping the route changes;

--- src/main/app/app.js
+++ src/main/app/app.js
@@ -1,24 +1,50 @@
 var happySurvey = angular.module('happySurvey',['ui.router', 'ngAnimate', 'ngSanitize', 'ngMaterial']);
 
+happySurvey.config(RoutesManager);
+
+RoutesManager.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesManager($stateProvider, $urlRouterProvider) {
+	
+	var signinState = {
+			name: 'signin',
+			url: '/signin',
+			templateUrl: '../views/signin.template.html'
+	}
+	
+	var homeState = {
+			name: 'home',
+			url: '/',
+			templateUrl: '../index.html'
+	}
+	
+	$stateProvider.state(signinState);
+	$stateProvider.state(homeState);
+	
+	$urlRouterProvider.otherwise('/');
+	
+}
+
+
 /*funktioniert noch nicht*/
-happySurvey.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise('/home');
+//happySurvey.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+//	
 	
-	$stateProvider
-	.state('public', {
-		abstract:true,
-		templateUrl:'../src/main/index.html'
-		//template:"<ui-view/>"
-	})
-	.state('public.site', {
-		url:'/start',
-		templateUrl:'../src/main/views/hellopage.html'
-	});
+//	
+//	$stateProvider
+//	.state('public', {
+//		abstract:true,
+//		templateUrl:'../src/main/index.html'
+//		//template:"<ui-view/>"
+//	})
+//	.state('public.site', {
+//		url:'/start',
+//		templateUrl:'../src/main/views/hellopage.html'
+//	});
+//	
+//	$stateProvider
+//	.state('private', {
+//		abstract:true,
+//		template:"<ui-view/>"
+//	});
 	
-	$stateProvider
-	.state('private', {
-		abstract:true,
-		template:"<ui-view/>"
-	});
-	
-}]);
+//}]);
