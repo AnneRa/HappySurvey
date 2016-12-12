27b339ae91e733685b0ef26e3f291c08e5042690
lawuc <lawuc@141.45.212.231>
2016-12-12 14:26:57 +0100

    *new structure

--- src/main/java/com/htw/test/config/WebSecurityConfig.java
+++ src/main/java/com/htw/test/config/WebSecurityConfig.java
@@ -1,50 +1,44 @@
 package com.htw.test.config;
-//package com.htw.analysis.happysurvey.config;
-/*package com.htw.analysis.happySurvey.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+/*import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import com.htw.analysis.backend.config.AntPathRequestMatcher;
+import com.htw.analysis.backend.config.AuthenticationManagerBuilder;
+import com.htw.analysis.backend.config.EnableGlobalMethodSecurity;
+import com.htw.analysis.backend.config.HttpSecurity;
+import com.htw.analysis.backend.config.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
 	
 	@Value("${admin.name}")
 	private String adminName;
 	
 	@Value("${admin.password}")
 	private String adminPassword;
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest().authenticated();
+        http
+                .formLogin().failureUrl("/signin?error")
+                .defaultSuccessUrl("/hello")
+                .loginPage("/signin")
+                .permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/signin")
+                .permitAll();
+    }
+ 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    	auth.inMemoryAuthentication().withUser(adminName).password(adminPassword).roles("USER");}
+}
+*/
 
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
 
-        RequestMatcher csrfRequestMatcher = new RequestMatcher() {
-
-            private RegexRequestMatcher requestMatcher = new RegexRequestMatcher("/api/v1", null);
-
-           @Override
-            public boolean matches(HttpServletRequest request) {
-                return requestMatcher.matches(request);
-            }
-        };
-
-        http.csrf().requireCsrfProtectionMatcher(csrfRequestMatcher);
-		
-		http.httpBasic().realmName("HappySurvey");
-		
-		http.authorizeRequests().antMatchers("/**").hasAnyRole("ADMIN");
-	}
-
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth)
-			throws Exception {
-		auth.inMemoryAuthentication().withUser(adminName).password(adminPassword).roles("ADMIN");
-	}
-
-}*/
