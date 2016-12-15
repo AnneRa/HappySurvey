b1cb94cafd12d4624d2932702e75619a55eccc9d
LaurenSchulze <lauren.schulze@gmx.de>
2016-12-15 15:18:57 +0100

    Code aufgeräumt

--- src/main/app/app.js
+++ src/main/app/app.js
@@ -1,20 +1,20 @@
 var happySurvey = angular.module('happySurvey',['ui.router', 'ngAnimate', 'ngSanitize', 'ngMaterial']);
 
-happySurvey.config(RoutesManager);
+//happySurvey.config(RoutesManager);
 
-RoutesManager.$inject = ['$stateProvider', '$urlRouterProvider'];
-function RoutesManager($stateProvider, $urlRouterProvider) {
+//RoutesManager.$inject = ['$stateProvider', '$urlRouterProvider'];
+//function RoutesManager($stateProvider, $urlRouterProvider) {
 	
-	var signinState = {
-			name: 'signin',
-			url: '/signin',
-			templateUrl: '../views/partial-signin.html'
-	}
+//	var signinState = {
+//			name: 'signin',
+//			url: '/signin',
+//			templateUrl: '../views/partial-signin.html'
+//	}
 	
 	
 	
-	$stateProvider.state(signinState);
+//	$stateProvider.state(signinState);
 	
-	$urlRouterProvider.otherwise('/');
+//	$urlRouterProvider.otherwise('/');
 	
-}
+//}
