7856171865ebabd083f6d05c05cba65bde3d6693
LaurenSchulze <lauren.schulze@gmx.de>
2016-12-17 13:42:02 +0100

    bla bla

--- src/main/app/app.js
+++ src/main/app/app.js
@@ -1,10 +1,13 @@
 var happySurvey = angular.module('happySurvey',['ui.router', 'ngAnimate', 'ngSanitize', 'ngMaterial']);
 
-//happySurvey.config(RoutesManager);
+happySurvey.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider ){
+	$urlRouterProvider.otherwise('/');
+
+
+	//happySurvey.config(RoutesManager);
 
 //RoutesManager.$inject = ['$stateProvider', '$urlRouterProvider'];
 //function RoutesManager($stateProvider, $urlRouterProvider) {
-	
 //	var signinState = {
 //			name: 'signin',
 //			url: '/signin',
@@ -18,3 +21,15 @@ var happySurvey = angular.module('happySurvey',['ui.router', 'ngAnimate', 'ngSan
 //	$urlRouterProvider.otherwise('/');
 	
 //}
+	$stateProvider
+		.state('private', {
+			url:'/',
+			templateUrl:'index.html',
+			template:'<ui-view></ui-view>'
+		})
+		.state('private.overviewAdmin', {
+			url:'/overviewAdmin',
+			templateUrl:'views/overviewAdmin.html'
+		});
+}]);
+
