53f40346a45d120f98bb7c206dbdd9931c534c45
Gesa Steensen <s0551690@htw-berlin.de>
2016-12-18 13:23:48 +0100

    sign in method with http service defined

--- src/main/app/controllers/loginController.js
+++ src/main/app/controllers/loginController.js
@@ -1,10 +1,31 @@
-happySurvey.controller('loginController', ['$scope', function($scope) {
+happySurvey.controller('loginController', ['$scope', '$http' , function($scope, $http) {
 	
 	'use strict';
 	
 	$scope.email = "";
     $scope.password = "";
     $scope.login = login;
+    
+    $scope.loginClicked = function(){
+    	
+    	$http({
+    		method: "GET",
+    		url: "/signin",
+    		params: {
+    			email: $scope.email,
+    			passwordinput: $scope.password 
+    		}
+    	}).then(
+    			
+    		function success(response) {
+    			// TODO implement 
+    		},
+    		
+    		function error(response) {
+    			// TODO implement
+    			console.log(response);
+    		});
+    }
     
 }]);
 
