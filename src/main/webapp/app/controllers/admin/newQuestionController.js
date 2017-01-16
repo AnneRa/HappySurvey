77f5c5f03b1d80d3670051eccea9cb8b4d2eba1e
Gesa Steensen <s0551690@htw-berlin.de>
2017-01-16 20:56:31 +0100

    NewQuestion restored

---
+++ src/main/webapp/app/controllers/admin/newQuestionController.js
@@ -0,0 +1,16 @@
+happySurvey.controller('newQuestionController', ['$scope', '$http' , function($scope, $http) {
+
+	'use strict';
+	
+	$scope.componentHandler = function() {
+		
+	}
+	$scope.createQuestionClicked = function() {
+		
+		//TODO implement
+		console.log("create question clicked");
+	}
+
+	
+
+}]);
