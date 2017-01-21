4ebdb7cb51660c7c2b0c20e09cb502e16343ed44
Gesa Steensen <s0551690@htw-berlin.de>
2017-01-21 18:59:46 +0100

    Einblenden der optionalen Attribute weiter entwickelt, Anzeigen typenSpezifischer Attribute

--- src/main/webapp/app/controllers/admin/newQuestionController.js
+++ src/main/webapp/app/controllers/admin/newQuestionController.js
@@ -5,12 +5,55 @@ happySurvey.controller('newQuestionController', ['$scope', '$http' , function($s
 	$scope.componentHandler = function() {
 		
 	}
+	
 	$scope.createQuestionClicked = function() {
 		
 		//TODO implement
 		console.log("create question clicked");
 	}
-
 	
+	$scope.onQuestionTypeChanged = function() {
+		
+		var typeSpecificAttributesContainer = document.getElementById('typeSpecificAttributes');
+		var linearScaleSpecificAttributesContainer = document.getElementById('linearScaleSpecificAttributesContainer');
+		var multipleChoiceAttributesContainer = document.getElementById('multipleChoiceAttributesContainer');
+		var dropDownAttributesContainer = document.getElementById('dropDownAttributesContainer');
+		var textFieldAttributesContainer = document.getElementById('textFieldAttributesContainer');
+		
+		typeSpecificAttributesContainer.style.display = 'none';
+		linearScaleSpecificAttributesContainer.style.display = 'none';
+		multipleChoiceAttributesContainer.style.display = 'none';
+		dropDownAttributesContainer.style.display = 'none';
+		textFieldAttributesContainer.style.display = 'none';
+		
+		var list = document.getElementById('questionType');
+		var selectedType = list[list.selectedIndex].value;
+		
+		switch(selectedType){
+		
+		case "linearScale": 
+			linearScaleSpecificAttributesContainer.style.display = 'block';
+			typeSpecificAttributesContainer.style.display = 'block';
+			break;
+		
+		case "multipleChoice":
+			multipleChoiceAttributesContainer.style.display = 'block';
+			typeSpecificAttributesContainer.style.display = 'block';
+			break;
+		
+		case "dropDown": 
+			dropDownAttributesContainer.style.display = 'block';
+			typeSpecificAttributesContainer.style.display = 'block';
+			break;
+		
+		case "textField":
+			textFieldAttributesContainer.style.display = 'block';
+			break;
+		
+		default:
+			break;
+		}
+	
+	}
 
 }]);
