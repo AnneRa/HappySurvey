f096f3e2e7ca45d3a4789eb42ae0781ca6f4ddec
Gesa Steensen <s0551690@htw-berlin.de>
2017-01-22 17:00:10 +0100

    dynamische Hinzufügen von weiteren Antwortmöglichkeiten (DropDown), Löschen zusätzlicher Fragen funktioniert noch nicht richtig

--- src/main/webapp/app/controllers/admin/newQuestionController.js
+++ src/main/webapp/app/controllers/admin/newQuestionController.js
@@ -12,6 +12,39 @@ happySurvey.controller('newQuestionController', ['$scope', '$http' , function($s
 		console.log("create question clicked");
 	}
 	
+	$scope.numberOfOptions = 2;
+	$scope.addOptionClicked = function() {
+		
+		$scope.numberOfOptions++;
+		
+		var htmlElementId = "mca_option_container" + $scope.numberOfOptions;
+		
+		 var optionNew = '<div id="' + htmlElementId + '" class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" id="optionNumber">' +
+					'<label class="mdl-textfield__label" for="mca_option' + $scope.numberOfOptions + '">Auswahl ' + $scope.numberOfOptions + '</label>' + 
+					'<input class="mdl-textfield__input" type="text" id="mca_option' + $scope.numberOfOptions + '">' +
+					'<button class="mdl-button mdl-js-button mdl-button--icon right-floating" ng-click="removeOptionClicked(' + htmlElementId + ')">' + 
+					  '<i class="material-icons">delete</i>' +
+					'</button>' +
+					'</div>';
+		 var dropDownAttributesElementsContainer = document.getElementById('dropDownAttributesElementsContainer');
+		 dropDownAttributesElementsContainer.innerHTML += optionNew;
+		 
+		 var elementNew = document.getElementById(htmlElementId);
+		 var body = angular.element(document.querySelector('body'));
+		 var $injector = body.injector();
+		 var $compile = $injector.get('$compile');
+		 $compile(elementNew)($scope);
+		 $scope.$apply();
+		 
+	}
+	
+	$scope.removeOptionClicked = function(optionId) {
+		
+		var parent = document.getElementById("dropDownAttributesElementsContainer");
+		var child = document.getElementById(optionId);
+		parent.removeChild(child);
+	}
+	
 	$scope.onQuestionTypeChanged = function() {
 		
 		var typeSpecificAttributesContainer = document.getElementById('typeSpecificAttributes');
