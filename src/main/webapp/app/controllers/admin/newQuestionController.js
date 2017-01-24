9dde27746217b4b27fb76b80bef07c480d4d3fe3
Gesa Steensen <s0551690@htw-berlin.de>
2017-01-24 15:59:58 +0100

    Deleting of further answer options work, as well as appending them correctly locally

--- src/main/webapp/app/controllers/admin/newQuestionController.js
+++ src/main/webapp/app/controllers/admin/newQuestionController.js
@@ -11,37 +11,18 @@ happySurvey.controller('newQuestionController', ['$scope', '$http' , function($s
 		//TODO implement
 		console.log("create question clicked");
 	}
+	$scope.options = ["Auswahl 1", "Auswahl 2"];
 	
-	$scope.numberOfOptions = 2;
 	$scope.addOptionClicked = function() {
 		
-		$scope.numberOfOptions++;
-		
-		var htmlElementId = "mca_option_container" + $scope.numberOfOptions;
-		
-		 var optionNew = '<div id="' + htmlElementId + '" class="mdl-textfield" id="optionNumber">' +
-					'<input class="mdl-textfield__input" type="text" placeholder="Auswahl ' + $scope.numberOfOptions + '" id="mca_option' + $scope.numberOfOptions + '">' +
-					'<button class="mdl-button mdl-js-button mdl-button--icon right-floating" ng-click="removeOptionClicked(' + htmlElementId + ')">' + 
-					  '<i class="material-icons">delete</i>' +
-					'</button>' +
-					'</div>';
-		 var dropDownAttributesElementsContainer = document.getElementById('choiceAttributesElementsContainer');
-		 dropDownAttributesElementsContainer.innerHTML += optionNew;
-		 
-		 var elementNew = document.getElementById(htmlElementId);
-		 var body = angular.element(document.querySelector('body'));
-		 var $injector = body.injector();
-		 var $compile = $injector.get('$compile');
-		 $compile(elementNew)($scope);
-		 $scope.$apply();
+		$scope.options.push("Auswahl " + ($scope.options.length +1));
 		 
 	}
 	
-	$scope.removeOptionClicked = function(optionId) {
-		
-		var parent = document.getElementById("choiceAttributesElementsContainer");
-		var child = document.getElementById(optionId);
-		parent.removeChild(child);
+	$scope.removeOptionClicked = function(item) {
+
+		  var index = $scope.options.indexOf(item);
+		   $scope.options.splice(index, 1);
 	}
 	
 	$scope.onQuestionTypeChanged = function() {
