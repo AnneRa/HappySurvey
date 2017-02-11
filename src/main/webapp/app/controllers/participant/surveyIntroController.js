fe85083e76bfb55d9773c2d1cf1b06867b455347
LaurenSchulze <lauren.schulze@gmx.de>
2017-02-11 13:09:28 +0100

    an teilnehmer ansicht arbeiten

--- src/main/webapp/app/controllers/participant/surveyIntroController.js
+++ src/main/webapp/app/controllers/participant/surveyIntroController.js
@@ -3,33 +3,19 @@ happySurvey.controller('surveyIntroController', ['$scope', '$http' , '$location'
 
 	'use strict';
 	
-	
-	//TODO: später wieder löschen
-	/*$scope.survey = {
-			"id":"tevztrug6",
-			"name":"Beispielumfrage",
-			"intro":"Ich würde mich freuen, wenn Sie an der Umfrage teilnehmen",
-			"description":"Die ist eine Beispielbeschreibung für die Beispielumfrage",
-			"categories": [{
-				"id":"sfskfssssssssa",
-				"name":"Sinn",
-				"questions":[{
-					"id":"jsfjpsfij",
-					"name":"Frage 1",
-					"type":"dropDown",
-					"options":["a","b","c"]
-				}]
-			},{},{}]
-	};*/
+	$scope.umfrage = [];
 	
 	$scope.startSurveyConduct = function(){
 		$location.path('conductSurvey/' + $routeParams.link).replace();
 	};
 	
-	$http.get("api/v1/getUmfrage/" + $routeParams.link)
-	.success( function ( response ) { 
-		$scope.survey = response;
-	})
+	$scope.showSurvey = function(link){
+		$http.get("http://localhost:8080/HappySurvey/api/v1/getUmfrage/" + link)
+		.success( function ( response ) { 
+			$scope.umfrage = response;
+		})
+	};
+	
 	
 }]);
 
